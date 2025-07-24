496a41d63683c2b7d76d3b62809bf89c2e6413db
baaghinitesh <baaghinitesh@gmail.com>
2025-07-24 12:10:39 +0530

    Added DSA problem solutions

---
+++ Easy level/sumOfDigits.java
@@ -0,0 +1,21 @@
+public class sumOfDigits {
+
+    // Method to calculate sum of digits
+    static int sumOfDigits(int n) {
+        int num = n;
+        int last;
+        int sum = 0;
+        while (num != 0) {
+            last = num % 10;   // Get last digit
+            sum = sum + last;  // Add to sum
+            num = num / 10;    // Remove last digit
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int number = 12345;  // Example number
+        int result = sumOfDigits(number);
+        System.out.println("Sum of digits of " + number + " is: " + result);
+    }
+}
