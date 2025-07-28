e56385eaea00fb78b6cbdad1fc79c93fd7365f45
baaghinitesh <baaghinitesh@gmail.com>
2025-07-28 15:52:38 +0530

    Added DSA Dailly Practice problem solutions

--- dataStructures/linckList/LinkList.java
+++ dataStructures/linckList/LinkList.java
@@ -1,55 +1,74 @@
-package dataStructures.linckList;
-
-public class LinkList {
-    // Node class representing each element in the linked list
-    static class Node {
+import java.util.Scanner;
+public class linkList {
+    class node {
         int data;
-        Node next;
+        node next;
 
-        Node(int data) {
+        //constructor to initialize node
+        node(int data){
             this.data = data;
             this.next = null;
         }
     }
 
-    private Node head; // Head of the linked list
-
-    // Method to insert a new node at the end of the linked list
-    public void insert(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
+    node head;
+    int countLinkList = 0; // keep tracking the number of nodes in the linked list
+    
+    // method to create a node and add it to the end of the list
+    public void addEnd(int data){
+        node newNode = new node(data);
+        if(head == null){
             head = newNode;
         } else {
-            Node current = head;
-            while (current.next != null) {
+            node current = head;
+            while(current.next != null){
                 current = current.next;
             }
             current.next = newNode;
         }
+        countLinkList++; // Increment count on each node addition
     }
 
-    // method to insert a new node at beginning of the linked list
-    public void insertAtBeginning(int data) {
-        Node newNode = new Node(data);
+    // method to create a node and add it to the beginning of the list
+    public void addStart(int data){
+        node newNode = new node(data);
         newNode.next = head;
         head = newNode;
+        countLinkList++; // Increment count on each node addition
     }
 
-    // Method to display the linked list
-    public void display() {
-        Node current = head;
-        while (current != null) {
-            System.out.print(current.data + " -> ");
+    // method to display the linked list
+    public void display(){
+        node current = head;
+        while(current !=null){
+            System.err.print(current.data + " --> ");
             current = current.next;
         }
-        System.out.println("null");
+        System.err.println("null");
     }
 
+    // method to get the count of nodes in the linked list
+    public int getCount() {
+        return countLinkList;
+    }
+
+    // main method to test the linked list
     public static void main(String[] args) {
-        LinkList list = new LinkList();
-        list.insert(1);
-        list.insert(2);
-        list.insert(3);
-        list.display(); // Output: 1 -> 2 -> 3 -> null
+        linkList list = new linkList();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of nodes you want to add:");
+        int n = input.nextInt();
+        for(int i=0; i<n; i++){
+            System.err.println("Enter data for node " +  (i+1) + ":");
+            int data = input.nextInt();
+            list.addEnd(data);
+        }
+
+        System.out.println("The linked list is:");
+        list.display();
+
+        System.out.println("The number of nodes in the linked list is: " + list.getCount());    
+        input.close();
+
     }
 }
